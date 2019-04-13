c31641ae824c297c1ce9788332221f8d2bee3e33
mateoguaman <mateo.guaman1998@gmail.com>
2019-04-12 23:55:33 -0400

    Experimenting around with motion

--- agent/scripts/pick_and_place.py
+++ agent/scripts/pick_and_place.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import rospy
 import moveit_commander
-
 import argparse
 import struct
 import sys
@@ -10,6 +9,7 @@ import time
 import random
 import os
 import subprocess, signal
+import tf
 
 import rospkg
 
@@ -32,7 +32,29 @@ from std_msgs.msg import (
     Empty,
 )
 
+from trajectory_msgs.msg import (
+    JointTrajectory,
+    JointTrajectoryPoint,
+)
+
+from moveit_msgs.msg import (
+    DisplayRobotState,
+    RobotState,
+    DisplayTrajectory,
+    Grasp,
+    Constraints,
+    JointConstraint,
+    PositionConstraint,
+    OrientationConstraint,
+    VisibilityConstraint,
+    GripperTranslation,
+    JointLimits,
+    LinkPadding,
+)
+
+
 robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
 arm_group = moveit_commander.MoveGroupCommander("manipulator")
 grp_group = moveit_commander.MoveGroupCommander("gripper")
 
@@ -63,12 +85,15 @@ def addnoise_pose():
     return pose
 
 def move_to_start():
-    arm_group.set_named_target('up')
+    #arm_group.set_named_target('up')
+    initial_joints = [1.6288508606798757e-05, -1.5999785607582009, -8.699362263797639e-05, -6.581909286129672e-05, -1.570796, 4.259259461747433e-05]
+
+    arm_group.set_joint_value_target(initial_joints)
     arm_group.go(wait=True)
     print("Moved to start")
 
 def gripper_open():
-    grp_group.set_joint_value_target([9.800441184282249e-05, -9.800441184282249e-05, 9.800441184282249e-05, 9.800441184282249e-05, -9.800441184282249e-05, 9.800441184282249e-05])
+    #grp_group.set_joint_value_target([9.800441184282249e-05, -9.800441184282249e-05, 9.800441184282249e-05, 9.800441184282249e-05, -9.800441184282249e-05, 9.800441184282249e-05])
     grp_group.go(wait=True)
 
 def gripper_close():
@@ -85,7 +110,7 @@ def approach(pose):
     approachPose.position.z = approachPose.position.z + hover_distance
     arm_group.set_pose_target(approachPose)
     arm_group.go(wait=True)
-	
+    
 def pick(pose):
     gripper_open()
     approach(pose)
@@ -101,32 +126,61 @@ def place(pose):
 
 def main():
     rospy.init_node('pick_and_place_node', anonymous=True)
-    
-    filename = '0'
-    num_of_run = 1  
 
     rospy.Subscriber("cover_pose", PoseStamped, setPoseCover)
     rospy.Subscriber("cup_pose", PoseStamped, setPoseCup)
 
 
-    while(CoverPose == None):
+    while (CoverPose == None):
         rospy.sleep(1)
 
-    item_to_pickup = CoverPose.pose
+
+    move_to_start()
+
+    testPose = copy.deepcopy(CoverPose.pose)
+    testPose.position.x = testPose.position.x - 0.4
+    testPose.position.z = testPose.position.z + 0.2
+    testPose.orientation.w = 1
+
+
+    testPoseLinear = copy.deepcopy(testPose)
+    testPose.position.x = testPose.position.x + 0.5
+
+
+
+
+
+    item_to_pickup = copy.deepcopy(CoverPose.pose)
+    item_to_pickup.position.z = item_to_pickup.position.z #+ 0.3
+    downOrientation = tf.transformations.quaternion_from_euler(0, 3.1415/2, 0)
+    item_to_pickup.orientation.x = downOrientation[0]
+    item_to_pickup.orientation.y = downOrientation[1]
+    item_to_pickup.orientation.z = downOrientation[2]
+    item_to_pickup.orientation.w = downOrientation[3]
+
     print(item_to_pickup)
 
-    for i in range(0,3):
+    # for i in range(0,3):
 
-        if(not rospy.is_shutdown()):
-            print("Picking up block")
-            pick(item_to_pickup)
-            placePose = addnoise_pose()
-            print("Placing block")
-            place(placePose)
-            gripper_open()
-            move_to_start()
-        else:
-            break   
+    #     if(not rospy.is_shutdown()):
+    #         print("Picking up block")
+    #         pick(item_to_pickup)
+    #         placePose = addnoise_pose()
+    #         print("Placing block")
+    #         place(placePose)
+    #         gripper_open()
+    #         move_to_start()
+    #     else:
+    #         break   
+
+    print("hi")
+
+    if not rospy.is_shutdown():
+
+        #move_to_pose(testPose)
+        #print("Moving Linearly")
+        #move_to_pose(testPoseLinear)
+        pick(item_to_pickup)
 
 
     # rospy.spin()
